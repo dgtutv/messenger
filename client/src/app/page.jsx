e1a294fcf709130dee70a70d31d0a37d70627b51
Daniel Todd <dgtutv@gmail.com>
2025-10-15 13:50:59 -0700

    Added basis for sockets on front and back end for messages within rooms

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,23 +1,53 @@
 "use client"
 
 import React, { useEffect, useState } from 'react'
+import { io } from 'socket.io-client';
 
+const socket = io.connect("http://localhost:8080"); //Change to actual URL when deploying
 
 function page() {
-  const [message, setMessage] = useState("loading...");
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+  const [room, setRoom] = useState("");
+
+  const joinRoom = () => {
+    if (room !== "") {
+      socket.emit("join_room", room);
+    }
+  }
 
   useEffect(() => {
     fetch("http://localhost:8080/api/home").then(
       response => response.json()
     ).then(
       data => {
-        setMessage(data.message);
+        setMessageReceived(data.message);
       }
     )
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageReceived(data.message);
+    });
+  }, [socket]);
+
+  const sendMessage = () => {
+    socket.emit("send_message", { message, room });
+  }
 
   return (
-    <div>{message}</div>
+    <div>
+      <p>{messageReceived}</p>
+      <input type="text" placeholder='Room ID' onChange={(event) => {
+        setRoom(event.target.value);
+      }} />
+      <button onClick={joinRoom}>Join</button>
+      <input type="text" placeholder='Message...' onChange={(event) => {
+        setMessage(event.target.value);
+      }} />
+      <button onClick={sendMessage}>Send Message</button>
+    </div>
   )
 }
 
--- server/server.js
+++ server/server.js
@@ -3,12 +3,35 @@ const app = express();
 const PORT = 8080;
 const cors = require("cors");
 
+//Required for socket.io
+const http = require("http");
+const { Server } = require("socket.io");
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",    //Set to react origin when deploying
+        methods: ["GET", "POST"],
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log(`User connected: ${socket.id}`);
+
+    socket.on("join_room", (data) => {  //Change to check if room exists, if not add to db
+        socket.join(data);
+    })
+
+    socket.on("send_message", (data) => {
+        socket.to(data.room).emit("receive_message", data);
+    });
+});
+
 app.use(cors());
 
 app.get("/api/home", (req, res) => {
     res.json({ message: "Hello world!" });
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`);
 });
