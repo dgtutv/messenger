c26618669210e87c225392dcce9165c6b398fad6
Daniel Todd <dgtutv@gmail.com>
2025-10-17 21:33:05 -0700

    DB issues fix 1

--- server/db/pool.js
+++ server/db/pool.js
@@ -2,5 +2,5 @@ const { Pool } = require("pg");
 const { USERNAME, PASSWORD, DBNAME } = process.env;
 // Again, this should be read from an environment variable
 module.exports = new Pool({
-    connectionString: `postgresql://${USERNAME}:${PASSWORD}@localhost:5432/${DBNAME}`
+    connectionString: `postgresql://${USERNAME}:${PASSWORD}@${DATABASE_URL}/${DBNAME}`
 });
