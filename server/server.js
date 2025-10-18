02d6c744466e06a3ff3f1252bb6ff3f5783cf1b0
Daniel Todd <dgtutv@gmail.com>
2025-10-17 20:45:22 -0700

    Added missing env variable

--- server/server.js
+++ server/server.js
@@ -94,7 +94,7 @@ const io = new Server(server, {
 
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
     credentials: true
 }));
 app.use(express.json());
